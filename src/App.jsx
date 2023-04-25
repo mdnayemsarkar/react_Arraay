dd45b215d69f333dcd678605e405798f3581ca9e
Nayeem <nayeem3334@gmail.com>
2023-04-25 22:26:14 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { ReactDOM } from "react-dom";
+
+
+
+function getArray() {
+  const  friends =["nayem","  ","sayem","  ","tohin","  ","ajmin"];
+  return  friends;
+}
+export default getArray;
+
+
